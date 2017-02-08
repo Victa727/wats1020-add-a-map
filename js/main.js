e40bf73f955b19ddaed4ba4aa678b24c7b0783cc
Victor Aguiluz <victa727@yahoo.com>
2017-02-08 11:44:20 -0800

    Update 2/8

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ $( document ).ready(function() {
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox://styles/victa727/ciyt9j8ol000s2sqvc9qvpy4s',
+    id: 'mapbox.mapbox-streets-v7',
     accessToken: 'pk.eyJ1IjoidmljdGE3MjciLCJhIjoiY2l5cGVrcWViMDAwaTJxbTQ1djlnNXYyOSJ9.Ye7V4mA-zL1_COWgi4nPow'
   }).addTo(mymap);
   
